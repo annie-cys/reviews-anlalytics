76a6e7a58fbdef1c36586daaa4e5f1d358c018ab
unknown <puddingcat945@gmail.com>
2019-01-21 16:41:02 +0800

    add avarage

--- read.py
+++ read.py
@@ -4,10 +4,10 @@ with open('reviews.txt', 'r') as f:
 		data.append(line.strip())
 		if len(data) % 1000 == 0 :# %餘數
 			print(len(data))
-print(len(data))# 印出data長度
+print('檔案讀取完了, 總共有',len(data),'筆資料')# 印出data長度
+total = 0
+for review in data:
+	total += len(review)
+    #迴圈跑一次讀一筆, 每次讀完都加總算出總長度
+print('每筆留言平均有',total/len(data),'字')
 
-print(data[0])
-print('--------------')
-print(data[1])
-
-#print很花時間
